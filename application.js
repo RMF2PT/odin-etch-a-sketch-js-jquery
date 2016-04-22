3f2401ec8274f42b6db0ee83c2b05c2ef5394a7a
Ricardo Ferreira <rmf2pt@gmail.com>
2016-04-22 14:23:58 +0100

    Start New button working

--- application.js
+++ application.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
-  newGrid(16, 16);
+  newGrid(16);
+  $(".startNew").click(function() {
+    $(".square").remove();
+    newGrid(16);
+  })
+});
+
+function newGrid(nSquares) {
+  var squareWidth = ($(".sketchpad").width() / nSquares);
+  var squareHeight= ($(".sketchpad").height() / nSquares);
+  for (var i = 0; i < nSquares; i++) {
+    for (var j = 0; j < nSquares; j++) {
+      var square = $("<div class='square'></div>");
+      $(".sketchpad").append(square);
+    }
+  }
+  $(".square").width(squareWidth).height(squareHeight);
+  hoverHighlight();
+}
+
+function hoverHighlight() {
   $(".square").hover(function() {
     $(this).addClass('highlight');
   });
-});
-
-function newGrid(gridHeight, gridWidth) {
-  var squareWidth = ($(".sketchpad").width() / gridWidth);
-  var squareHeight= ($(".sketchpad").height() / gridHeight);
-  for (var i = 0; i < gridHeight; i++) {
-    for (var j = 0; j < gridWidth; j++) {
-      var square = $("<div class='square'></div>");
-      $(".sketchpad").append(square);
-
-    }
-    $(".sketchpad").append("<br>");
-  }
-  $(".square").width(squareWidth).height(squareHeight);
 }

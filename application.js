f643eb300df38ce879a4e6dedd92f58af513d1d8
Ricardo Ferreira <rmf2pt@gmail.com>
2016-04-22 12:17:15 +0100

    Add working grid and hover effect. Need to work on margin bottom

--- application.js
+++ application.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   newGrid(16, 16);
-
+  $(".square").hover(function() {
+    $(this).addClass('highlight');
+  });
 });
 
 function newGrid(gridHeight, gridWidth) {
-  
+  var squareWidth = ($(".sketchpad").width() / gridWidth);
+  var squareHeight= ($(".sketchpad").height() / gridHeight);
   for (var i = 0; i < gridHeight; i++) {
     for (var j = 0; j < gridWidth; j++) {
-      var square = $("<div class='square'>" + i + " " + j +"</div>");
+      var square = $("<div class='square'></div>");
       $(".sketchpad").append(square);
+
     }
     $(".sketchpad").append("<br>");
   }
+  $(".square").width(squareWidth).height(squareHeight);
 }
